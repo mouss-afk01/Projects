a63b99d2d7fa576b417c58fd64a965ca03dcbb87
mouss-afk01 <afakkir.oussama+github@protonmail.com>
2026-09-24 11:41:28 +0200

    Update Files

--- OpenGL/TEMPLATE_OPENGL_FOLDER/assets/Shaders/fragment1.fs
+++ OpenGL/TEMPLATE_OPENGL_FOLDER/assets/Shaders/fragment1.fs
@@ -1,11 +1,10 @@
 #version 330 core
 out vec4 FragColor;
+
 in vec3 ourColor;
 in vec3 ourPosition;
 
 void main()
 {
-	// exercise 6.8.3:
-	// why is the buttom-left side dark : because all the values are or undefined in the colorspectrum or zero, which means they represent black.
 	FragColor = vec4(ourPosition, 1.0f);
 }
--- OpenGL/TEMPLATE_OPENGL_FOLDER/src/main.cpp
+++ OpenGL/TEMPLATE_OPENGL_FOLDER/src/main.cpp
@@ -52,18 +52,38 @@ int main()
 		Shader(	"assets/Shaders/vector1.vs", 
 			"assets/Shaders/fragment1.fs");
 
-	float vertices[] = {
-	}; 
+  float vertices[] = {
+    0.5f, 0.5f, 0.0f, // top right
+    0.5f, -0.5f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f, // bottom left
+    -0.5f, 0.5f, 0.0f // top left
+  };
+  // ADDED:
+  unsigned int indices[] = { // note that we start from 0!
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
+  };
 
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	// load vertices into gpu-memory:
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)(0));
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
 	// uncomment this call to draw in wireframe polygons.
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
@@ -79,16 +99,9 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// draw our first triangle
 		shaderProgram.use();
-		// exercise 6.8.2: move triangles by an offset:
-		//shaderProgram.setInt("h_offset", 0.5);
-		//
-		//glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		//glBindVertexArray(0); // no need to unbind it every time 
-
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// --------------------------------------------------------
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void *)(3 * sizeof(float)));
+    glBindVertexArray(VAO);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
