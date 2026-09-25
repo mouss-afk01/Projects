82e79734b2806c73fe12c6070d4a597ac3bcccc8
mouss-afk01 <afakkir.oussama+github@protonmail.com>
2026-09-25 23:22:25 +0200

    commit ending

---
+++ OpenGL/test/build/glad.d
@@ -0,0 +1,6 @@
+build/glad.o: \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/SDKSettings.json \
+  src/glad.c include/glad/glad.h include/KHR/khrplatform.h
+/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/SDKSettings.json:
+include/glad/glad.h:
+include/KHR/khrplatform.h:
