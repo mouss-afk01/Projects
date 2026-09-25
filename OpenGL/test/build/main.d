db26f2859608c12763d35d5eef0a7c591342f1b2
mouss-afk01 <afakkir.oussama+github@protonmail.com>
2026-09-25 23:28:14 +0200

    war against buildingss

--- OpenGL/test/build/main.d
+++
@@ -1,9 +0,0 @@
-build/main.o: \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/SDKSettings.json \
-  src/main.cpp include/glad/glad.h include/KHR/khrplatform.h \
-  /opt/homebrew/Cellar/glfw/3.5.1/include/GLFW/glfw3.h include/shader.h
-/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/SDKSettings.json:
-include/glad/glad.h:
-include/KHR/khrplatform.h:
-/opt/homebrew/Cellar/glfw/3.5.1/include/GLFW/glfw3.h:
-include/shader.h:
